ce9c6defbf263be7e77a58157c09a2fa4f706e93
KonscienceGit <nicolaspoin@gmail.com>
2018-02-24 15:50:56 +0100

    Added CSS file, displacement map, reworked normal map

--- js/actualscript.js
+++ js/actualscript.js
@@ -19,7 +19,6 @@ var main = function () {
     var cornerNE, cornerNW, cornerSE, cornerSW, ship, earth, mars, sun, skybox;
 
     init();
-
     animate();
 
     function init() {
@@ -32,107 +31,85 @@ var main = function () {
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
         controls = new THREE.OrbitControls( camera );
-        //Anisotrop filtering, setting to the max possible
+    //Anisotrop filtering, setting to the max possible
         var maxAnisotropy = renderer.capabilities.getMaxAnisotropy();
 
-
     //images and textures
-    var textureLoader = new THREE.TextureLoader();//https://www.solarsystemscope.com/images/textures/full/2k_earth_daymap.jpg
-    var colorMapEarth = textureLoader.load("earth.jpg");
-    colorMapEarth.anisotropy = maxAnisotropy;
-    var specularMapEarth = textureLoader.load("earth_specular_map.jpg");
-    var normalMapEarth = textureLoader.load("earth_normal_map.jpg");
-    normalMapEarth.anisotropy = maxAnisotropy;
+        var textureLoader = new THREE.TextureLoader();//https://www.solarsystemscope.com/images/textures/full/2k_earth_daymap.jpg
+        var colorMapEarth = textureLoader.load("maps/earth.jpg");
+        colorMapEarth.anisotropy = maxAnisotropy;
+        var specularMapEarth = textureLoader.load("maps/earth_specular_map.jpg");
+        var normalMapEarth = textureLoader.load("maps/earth_normal_map.jpg");
+        normalMapEarth.anisotropy = maxAnisotropy;
 
-    var colorMapMars = textureLoader.load("mars.jpg");
-    colorMapMars.anisotropy = maxAnisotropy;
-    var normalMapMars = textureLoader.load("mars_normal_map.jpg");
-    colorMapMars.anisotropy = maxAnisotropy;
+        var colorMapMars = textureLoader.load("maps/mars.jpg");
+        colorMapMars.anisotropy = maxAnisotropy;
+        var normalMapMars = textureLoader.load("maps/mars_norm_map.png");
+        colorMapMars.anisotropy = maxAnisotropy;
+        var displacementMapMars = textureLoader.load("maps/mars_disp_map.png");
+        colorMapMars.anisotropy = maxAnisotropy;
 
-    var colorMapSkybox = textureLoader.load("milkyway.jpg");
-
+        var colorMapSkybox = textureLoader.load("maps/milkyway.jpg");
+        colorMapSkybox.anisotropy = maxAnisotropy;
 
     //materials engineering
-    var matEarth = new THREE.MeshPhongMaterial({
-        color: 0xaaaaaa,
-        specular: 0x333333,
-        shininess: 5,
-        map: colorMapEarth,
-        specularMap: specularMapEarth,
-        normalMap: normalMapEarth
-    });
-    var matMars = new THREE.MeshPhongMaterial({
-        color: 0xaaaaaa,
-        specular: 0x000000,
-        shininess: 0,
-        map: colorMapMars,
-        normalMap: normalMapMars
-    });
-    var matSkybox = new THREE.MeshBasicMaterial({
-        map: colorMapSkybox,
-        side: THREE.BackSide
-    });
+        var matEarth = new THREE.MeshPhongMaterial({
+            color: 0xaaaaaa,
+            shininess: 25,
+            map: colorMapEarth,
+            specularMap: specularMapEarth,
+            normalMap: normalMapEarth
+        });
+        var matMars = new THREE.MeshPhongMaterial({
+            color: 0xaaaaaa,
+            specular: 0x000000,
+            shininess: 0,
+            map: colorMapMars,
+            normalMap: normalMapMars,
+            displacementMap: displacementMapMars,
+            displacementScale: 0.1
+        });
+        var matSkybox = new THREE.MeshBasicMaterial({
+            map: colorMapSkybox,
+            side: THREE.BackSide
+        });
 
-    var matShip = new THREE.MeshBasicMaterial({color: 0xffffff});
-    var matRed = new THREE.MeshBasicMaterial({color: 0xff0000});
-    var matGreen = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    var matBlue = new THREE.MeshBasicMaterial({color: 0x0000ff});
-    var matWhite = new THREE.MeshBasicMaterial({color: 0xffffff});
-    var matToon = new THREE.MeshToonMaterial({color: 0xaaaaaa});
+        var matWhite = new THREE.MeshBasicMaterial({color: 0xffffff});
+
 
     //geometry
-    var geomCube = new THREE.BoxGeometry(1, 1, 1);
-    var geomSphere = new THREE.SphereGeometry (1,48,32);
-    var geomSkybox = new THREE.SphereGeometry (10,24,16);
+        var geomCube = new THREE.BoxGeometry(1, 1, 1);
+        var geomSphere = new THREE.SphereGeometry (1,300,200);
+        var geomSkybox = new THREE.SphereGeometry (10,24,16);
 
     //mesh positioning
-    cornerNE = new THREE.Mesh(geomCube, matRed);
-    cornerNE.position.x = 8;
-    cornerNE.position.y = 4.5;
-    scene.add(cornerNE);
-    cornerNW = new THREE.Mesh(geomCube, matGreen);
-    cornerNW.position.x = -8;
-    cornerNW.position.y = 4.5;
-    scene.add(cornerNW);
-    cornerSE = new THREE.Mesh(geomCube, matBlue);
-    cornerSE.position.x = 8;
-    cornerSE.position.y = -4.5;
-    scene.add(cornerSE);
-    cornerSW = new THREE.Mesh(geomCube, matWhite);
-    cornerSW.position.x = -8;
-    cornerSW.position.y = -4.5;
-    scene.add(cornerSW);
-    ship = new THREE.Mesh(geomCube, matShip);
-    ship.position.x = 0;
-    ship.position.y = -4.5;
-    scene.add(ship);
-    earth = new THREE.Mesh(geomSphere, matEarth);
-    earth.position.x = 0;
-    earth.position.y = 2.5;
-    scene.add(earth);
-    mars = new THREE.Mesh(geomSphere, matMars);
-    mars.position.x = 0;
-    mars.position.y = 0;
-    scene.add(mars);
-    sun = new THREE.Mesh(geomSphere, matWhite);
-    sun.position.x = 10;
-    sun.position.y = 0;
-    scene.add(sun);
-    skybox = new THREE.Mesh(geomSkybox, matSkybox);
-    skybox.material.depthWrite = false;
-    skybox.renderOrder = -999;
+        earth = new THREE.Mesh(geomSphere, matEarth);
+        earth.position.x = 0;
+        earth.position.y = 2.5;
+        scene.add(earth);
+        mars = new THREE.Mesh(geomSphere, matMars);
+        mars.position.x = 0;
+        mars.position.y = 0;
+        scene.add(mars);
+        sun = new THREE.Mesh(geomSphere, matWhite);
+        sun.position.x = 10;
+        sun.position.y = 0;
+        scene.add(sun);
+        skybox = new THREE.Mesh(geomSkybox, matSkybox);
+        skybox.material.depthWrite = false;
+        skybox.renderOrder = -999;
 
-    scene.add(skybox);
+        scene.add(skybox);
 
 
     //light
-    //renderer.gammaInput = true;
-    //renderer.gammaOutput = true;
-    //renderer.toneMapping = THREE.ReinhardToneMapping;
-    renderer.toneMappingExposure = 2; //affect exposure
-    sunlight = new THREE.PointLight( 0xffffff, 1, 50 );
-    sunlight.position.x=10;
-    scene.add( sunlight );
+        //renderer.gammaInput = true;
+        //renderer.gammaOutput = true;
+        //renderer.toneMapping = THREE.ReinhardToneMapping;
+        renderer.toneMappingExposure = 2; //affect exposure
+        sunlight = new THREE.PointLight( 0xffffff, 1, 50 );
+        sunlight.position.x=10;
+        scene.add( sunlight );
     }
 
     function animate() {
